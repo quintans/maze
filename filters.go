1312024a79f12192087fafaa923cb0f3ed8f3f7e
Paulo Quintans Pereira <paulo.quintans@gmail.com>
2016-11-01 22:09:18 +0000

    added some general filters

---
+++ filters.go
@@ -0,0 +1,31 @@
+package maze
+
+import (
+	"github.com/quintans/maze"
+	"github.com/quintans/toolkit/web"
+)
+
+// ResponseBuffer buffers the response, permiting setting headers after starting writing the response.
+func ResponseBuffer(c maze.IContext) error {
+	var rec = web.NewBufferedResponse()
+	var w = c.GetResponse()
+	// passing a buffer instead of the original RW
+	c.SetResponse(rec)
+	// restores the original response, even in the case of a panic
+	defer func() {
+		c.SetResponse(w)
+	}()
+	err := c.Proceed()
+	if err == nil {
+		rec.Flush(w)
+	}
+
+	return err
+}
+
+func StaticGz(dir string) func(c maze.IContext) error {
+	var rs = web.ResourcesHandler(dir)
+	return func(c maze.IContext) error {
+		rs.ServeHTTP(c.GetResponse(), c.GetRequest())
+	}
+}
